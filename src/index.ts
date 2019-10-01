96c3155b57a694d9f7e2f0a8b0d9747b9470e571
Jonathan Brown <jbrown@mix-blockchain.org>
2019-10-01 13:40:36 +0700

    Create web server.

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+
+let app = express()
+let port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
