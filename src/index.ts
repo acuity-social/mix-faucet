cf2541e60ccd09f809af0514194c4ebbe6092881
Jonathan Brown <jbrown@mix-blockchain.org>
2019-10-01 14:54:51 +0700

    Connect to blockchain.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
+import Web3 from 'web3'
+import net from 'net'
 import express from 'express'
+
+let ipcPath = '~/.local/share/io.parity.ethereum/jsonrpc.ipc'
+let web3 = new Web3(new Web3.providers.IpcProvider(ipcPath, net))
 
 let app = express()
 let port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+async function start() {
+	let parityIpcPath = process.env['HOME'] + '/.config/MIX Acuity/parity.ipc'
+//	let parityIpcPath = process.env['HOME'] + '/.local/share/io.parity.ethereum/jsonrpc.ipc'
+	let web3 = new Web3(new Web3.providers.IpcProvider(parityIpcPath, net))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+	let blockNumber = await web3.eth.getBlockNumber()
+	console.log('Block: ' + blockNumber.toLocaleString())
+
+	app.get('/:address', (req, res) => {
+		console.log(req.params.address)
+		res.send('Hello World!')
+	})
+
+	app.listen(port, () => console.log(`MIX Faucet listening on port ${port}!`))
+}
+
+start()
