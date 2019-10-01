774ce505724e965b76aa5f4b435ff4a643abd5b5
Jonathan Brown <jbrown@mix-blockchain.org>
2019-10-01 15:14:15 +0700

    Initiaize db.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import Web3 from 'web3'
 import net from 'net'
+import levelup from 'levelup'
+import leveldown from 'leveldown'
 import express from 'express'
 
-let ipcPath = '~/.local/share/io.parity.ethereum/jsonrpc.ipc'
-let web3 = new Web3(new Web3.providers.IpcProvider(ipcPath, net))
-
-let app = express()
-let port = 3000
 
 async function start() {
 	let parityIpcPath = process.env['HOME'] + '/.config/MIX Acuity/parity.ipc'
@@ -15,6 +12,13 @@ async function start() {
 
 	let blockNumber = await web3.eth.getBlockNumber()
 	console.log('Block: ' + blockNumber.toLocaleString())
+
+	let dbPath = process.env['HOME'] + '/.faucet.db'
+	console.log('Initializing database: ' + dbPath)
+	var db = levelup(leveldown(dbPath))
+
+	let app = express()
+	let port = 3000
 
 	app.get('/:address', (req, res) => {
 		console.log(req.params.address)
